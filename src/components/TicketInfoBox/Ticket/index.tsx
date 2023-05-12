f7292d90dd9fccbffdf4dad154a4d0349d863e70
nahyyun <nahyuun@naver.com>
2023-05-12 17:50:16 +0900

    style: 티켓 번호 css 수정

--- src/components/TicketInfoBox/Ticket/index.tsx
+++ src/components/TicketInfoBox/Ticket/index.tsx
@@ -9,7 +9,7 @@ const Ticket = ({ ticket, isToggleOn }: TicketProps) => {
   return (
     <li>
       <span className="mx-1 text-4xl">🎟️ </span>
-      {isToggleOn && <span>{ticket.join(",")}</span>}
+      {isToggleOn && <span className="text-3xl">{ticket.join(", ")}</span>}
     </li>
   );
 };
